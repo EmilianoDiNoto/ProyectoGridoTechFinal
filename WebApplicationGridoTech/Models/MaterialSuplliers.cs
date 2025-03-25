1198c3f3a645c08d3271e6a4210ea0f1f8dca512
EmmaGonzalezCastro <gonzalezcastro.emman@gmail.com>
2025-03-25 17:07:29 -0300

    modificaciones en API 25/3/25

---
+++ WebApplicationGridoTech/Models/MaterialSuplliers.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace WebApplicationGridoTech.Models
+{
+    public class MaterialSuppliersRepository
+    {
+        private string connectionString;
+
+        public MaterialSuppliersRepository()
+        {
+            // Asegúrate de tener "YourConnectionStringName" configurado en web.config o appsettings.json
+            connectionString = ConfigurationManager.ConnectionStrings["YourConnectionStringName"].ConnectionString;
+        }
+
+        //Buscar Orden por nombre de producto
+        public DataTable SelectByMaterialSuppliers(string materialname)
+        {
+            string sqlSentencia = "SP_Get_MatProv";
+
+            using (SqlConnection sqlCnn = new SqlConnection(connectionString))
+            {
+                sqlCnn.Open();
+                using (SqlCommand sqlCom = new SqlCommand(sqlSentencia, sqlCnn))
+                {
+                    sqlCom.CommandType = CommandType.StoredProcedure;
+                    sqlCom.Parameters.AddWithValue("@MATERIAL", materialname);
+
+                    DataSet ds = new DataSet();
+                    using (SqlDataAdapter da = new SqlDataAdapter(sqlCom))
+                    {
+                        da.Fill(ds);
+                    }
+
+                    return ds.Tables.Count > 0 ? ds.Tables[0] : new DataTable();
+                }
+            }
+        }
+
+    }
+
+    public class MaterialSupplier
+    {
+        public string PROVEEDOR { get; set; }
+    }
+}
--- WebApplicationGridoTech/Models/WorkOrders.cs
+++ WebApplicationGridoTech/Models/WorkOrders.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
@@ -7,65 +8,58 @@ using System.Web;
 
 namespace WebApplicationGridoTech.Models
 {
-    public class WorkOrders
+    public class WorkOrdersRepository
     {
-        #region Atributos
-        // Conexión a Base de Datos EMMA
-        string conectionString = @"Data Source=LAPTOP-OJ158TC8 ;Initial Catalog=GridoTech ; Integrated Security= True ";
+        private string connectionString;
 
-        //// Conexión a Base de Datos EMI
-        //string conectionString = @"Data Source=EMI-PC\EMI_PC_SERVER;Initial Catalog=GridoTech ; Integrated Security= True ";
-
-        #endregion
-
-        #region Propiedades
-        public int OT { get; set; }
-        public int PRODUCTOID { get; set; }
-        public int CODIGO { get; set; }
-        public string PRODUCTO { get; set; }
-        public int DEMANDA { get; set; }
-        public string UM { get; set; }
-        public DateTime FECHACREADA { get; set; }
-        public DateTime FECHAMODIFICADA { get; set; }
-        public string ESTADO { get; set; }
-        public DateTime FECHAELABORACION { get; set; }
-
-
-
-
-
-        #endregion
-
-        #region Metodos
-        public DataTable SelectAll()
+        public WorkOrdersRepository()
         {
-            string sqlSentencia = "SP_GetAll_WorkOrder";
-
-            SqlConnection sqlCnn = new SqlConnection();
-            sqlCnn.ConnectionString = conectionString;
-
-            sqlCnn.Open();
-
-            SqlCommand sqlCom = new SqlCommand(sqlSentencia, sqlCnn);
-            sqlCom.CommandType = CommandType.StoredProcedure;
-
-            DataSet ds = new DataSet();
-
-            SqlDataAdapter da = new SqlDataAdapter();
-            da.SelectCommand = sqlCom;
-            da.Fill(ds);
-
-            sqlCnn.Close();
-
-            return ds.Tables[0];
-
+            // Asegúrate de tener "YourConnectionStringName" configurado en web.config o appsettings.json
+            connectionString = ConfigurationManager.ConnectionStrings["YourConnectionStringName"].ConnectionString;
         }
 
+        // Nuevo método para obtener todos los movimientos del almacen
+        public List<WorkOrders> GetAllWorkOrders()
+        {
+            List<WorkOrders> WorkOrders = new List<WorkOrders>();
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                using (SqlCommand command = new SqlCommand("SP_GetAll_WorkOrder", connection))
+                {
+                    command.CommandType = CommandType.StoredProcedure;
+
+                    connection.Open();
+
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            WorkOrders.Add(new WorkOrders
+                            {
+                                OT = Convert.ToInt32(reader["OT"]),
+                                CODIGO = Convert.ToInt32(reader["CODIGO"]),
+                                PRODUCTO = reader["PRODUCTO"].ToString(),
+                                DEMANDA = Convert.ToInt32(reader["DEMANDA"]),
+                                UM = reader["UM"].ToString(),
+                                ESTADO = reader["ESTADO"].ToString(),
+                                FECHAELABORACION = Convert.ToDateTime(reader["FECHAELABORACION"]),
+
+                            });
+                        }
+                    }
+                }
+            }
+
+            return WorkOrders;
+        }
+
+        //Buscar Orden por nombre de producto
         public DataTable SelectByProductName(string productName)
         {
             string sqlSentencia = "SP_Get_WorkOrderName";
 
-            using (SqlConnection sqlCnn = new SqlConnection(conectionString))
+            using (SqlConnection sqlCnn = new SqlConnection(connectionString))
             {
                 sqlCnn.Open();
                 using (SqlCommand sqlCom = new SqlCommand(sqlSentencia, sqlCnn))
@@ -84,70 +78,73 @@ namespace WebApplicationGridoTech.Models
             }
         }
 
-
-        public void Insert()
+        //Buscar Orden por nombre de producto
+        public DataTable SelectByProductStatus(string productStatus)
         {
+            string sqlSentencia = "SP_GetTop_WorkOrder";
 
-            string sqlSentencia = "SP_Insert_WorkOrder";
-            SqlConnection sqlCnn = new SqlConnection();
-            sqlCnn.ConnectionString = conectionString;
+            using (SqlConnection sqlCnn = new SqlConnection(connectionString))
+            {
+                sqlCnn.Open();
+                using (SqlCommand sqlCom = new SqlCommand(sqlSentencia, sqlCnn))
+                {
+                    sqlCom.CommandType = CommandType.StoredProcedure;
+                    sqlCom.Parameters.AddWithValue("@status", productStatus);
 
-            SqlCommand sqlCom = new SqlCommand(sqlSentencia, sqlCnn);
-            sqlCom.CommandType = CommandType.StoredProcedure;
+                    DataSet ds = new DataSet();
+                    using (SqlDataAdapter da = new SqlDataAdapter(sqlCom))
+                    {
+                        da.Fill(ds);
+                    }
 
-            sqlCom.Parameters.Add("@PRODUCTOID", SqlDbType.Int).Value = PRODUCTOID;
-            sqlCom.Parameters.Add("@CODIGO", SqlDbType.Int).Value = CODIGO;
-            //sqlCom.Parameters.Add("@PRODUCTO", SqlDbType.NVarChar).Value = PRODUCTO;
-            sqlCom.Parameters.Add("@DEMANDA", SqlDbType.Int).Value = DEMANDA;
-            sqlCom.Parameters.Add("@UM", SqlDbType.NVarChar).Value = UM;
-            sqlCom.Parameters.Add("@FECHACREADA", SqlDbType.DateTime).Value = FECHACREADA;
-            sqlCom.Parameters.Add("@FECHAMODIFICADA", SqlDbType.DateTime).Value = FECHAMODIFICADA;
-            sqlCom.Parameters.Add("@ESTADO", SqlDbType.NVarChar).Value = ESTADO;
-            sqlCom.Parameters.Add("@FECHAELABORACION", SqlDbType.Date).Value = FECHAELABORACION;
-
-
-            sqlCnn.Open();
-
-            var res = sqlCom.ExecuteNonQuery();
-
-            sqlCnn.Close();
-
+                    return ds.Tables.Count > 0 ? ds.Tables[0] : new DataTable();
+                }
+            }
         }
 
-        public void Actualizar()
+
+
+
+
+        // Nuevo método para insertar un movimiento en WorkOrders
+        public void InsertWorkOrders(int productid, int codigo, int cantidad, string um, DateTime fechacreada, DateTime fechamodificada, string estado, DateTime fechaelaboracion)
         {
-            string sqlSentencia = "SP_Update_WorkOrders";
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                using (SqlCommand command = new SqlCommand("SP_Insert_WorkOrder", connection))
+                {
+                    command.CommandType = CommandType.StoredProcedure;
 
+                    // Agregar parámetros al comando
+                    command.Parameters.AddWithValue("@PRODUCTOID", productid);
+                    command.Parameters.AddWithValue("@CODIGO", codigo);
+                    command.Parameters.AddWithValue("@DEMANDA", cantidad);
+                    command.Parameters.AddWithValue("@UM", um);
+                    command.Parameters.AddWithValue("@FECHACREADA", fechacreada);
+                    command.Parameters.AddWithValue("@FECHAMODIFICADA", fechamodificada);
+                    command.Parameters.AddWithValue("@ESTADO", estado);
+                    command.Parameters.AddWithValue("@FECHAELABORACION", fechaelaboracion);
 
-            SqlConnection sqlCnn = new SqlConnection();
-            sqlCnn.ConnectionString = conectionString;
-
-
-            SqlCommand sqlCom = new SqlCommand(sqlSentencia, sqlCnn);
-            sqlCom.CommandType = CommandType.StoredProcedure;
-            sqlCom.Parameters.Add("@OT", SqlDbType.Int).Value = OT;
-            sqlCom.Parameters.Add("@PRODUCTOID", SqlDbType.Int).Value = PRODUCTOID;
-            sqlCom.Parameters.Add("@CODIGO", SqlDbType.Int).Value = CODIGO;
-            //sqlCom.Parameters.Add("@PRODUCTO", SqlDbType.NVarChar).Value = PRODUCTO;
-            sqlCom.Parameters.Add("@DEMANDA", SqlDbType.Int).Value = DEMANDA;
-            sqlCom.Parameters.Add("@UM", SqlDbType.NVarChar).Value = UM;
-            sqlCom.Parameters.Add("@FECHACREADA", SqlDbType.DateTime).Value = FECHACREADA;
-            sqlCom.Parameters.Add("@FECHAMODIFICADA", SqlDbType.DateTime).Value = FECHAMODIFICADA;
-            sqlCom.Parameters.Add("@ESTADO", SqlDbType.NVarChar).Value = ESTADO;
-            sqlCom.Parameters.Add("@FECHAELABORACION", SqlDbType.DateTime).Value = FECHAELABORACION;
-
-            sqlCnn.Open();
-
-
-            var res = sqlCom.ExecuteNonQuery();
-
-
-            sqlCnn.Close();
-
+                    connection.Open();
+                    command.ExecuteNonQuery(); // Ejecuta el procedimiento almacenado
+                }
+            }
         }
 
-        #endregion
+    }
 
 
+    public class WorkOrders
+    {
+        public int OT { get; set; }
+        public int PRODUCTOID { get; set; }
+        public int CODIGO { get; set; }
+        public string PRODUCTO { get; set; }
+        public int DEMANDA { get; set; }
+        public string UM { get; set; }
+        public DateTime FECHACREADA { get; set; }
+        public DateTime FECHAMODIFICADA { get; set; }
+        public string ESTADO { get; set; }
+        public DateTime FECHAELABORACION { get; set; }
     }
 }
