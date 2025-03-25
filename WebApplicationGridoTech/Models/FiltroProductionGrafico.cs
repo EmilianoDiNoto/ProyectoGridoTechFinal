3b006357377381bb71f8bf27fb9bd8738e71f310
Emiliano Di Noto <emi04014@gmail.com>
2025-03-25 15:24:46 -0300

    Subo los cambios de los filtros de los gráficos del home.

---
+++ WebApplicationGridoTech/Models/FiltroProductionGrafico.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration; // Necesario para acceder a la cadena de conexión
+
+namespace WebApplicationGridoTech.Models
+{
+  
+    public class FiltroProductionGrafico
+    {
+        public DateTime Fecha { get; set; }
+        public string ProductName { get; set; }
+        public int Cantidad { get; set; }
+        public int Demanda { get; set; }
+    }
+
+
+}
